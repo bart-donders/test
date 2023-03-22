11205cf5f31cbafa3d4ad49ed94212e29cf232db
Bart Donders - Calculus Software <bart.donders@calculus.nl>
2023-03-22 11:06:30 +0100

    Console app for testing GitHub Actions.

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string message = "Hello, World!";
+
+//Debug.WriteLine(message);
+//Debug.Assert(false, "Test Debug.Assert");
+
+Console.WriteLine(message);
